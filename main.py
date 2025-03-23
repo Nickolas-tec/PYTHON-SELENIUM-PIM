e600188db23db8443a2e468f89445431a9e6bd85
Nickolas <112489723+Nickolas-tec@users.noreply.github.com>
2025-03-22 21:10:52 -0300

    Update main.py
    Até o momento o código acessa o sistema SIDRA IBGE seleciona indicadores; variáveis e periodos.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
-# CARREGAMENTO DE BIBLITOECAS
+
+#####################################################################################################################
+# CARREGAMENTO DE BIBLITOECAS E MODULOS
 import time
 import selenium
 import selenium.webdriver
@@ -7,6 +9,8 @@ from selenium.webdriver.common.by import By
 from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+#####################################################################################################################
 
 # O OBJETO DRIVER RECEBE O WEBDRIVER DO CHROME DO SELENIUM
 driver = selenium.webdriver.Chrome()
@@ -24,12 +28,20 @@ sleep(1)
 navegador.get(url)
 
 
+#####################################################################################################################
+
+# ESTE TRECHO SELECIONA O FILTRO DE PESQUISAS
+
 # AGUARDA ATÉ QUE O ELEMENTO COM A CLASSE "dropdown-toggle" ESTEJA PRESENTE NO DOM E ARMAZENA ELE NA VARIÁVEL
 botao_pesquisas = WebDriverWait(navegador, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, "dropdown-toggle"))
 )
 # EM SEGUIDA, REALIZA O CLIQUE NO ELEMENTO IDENTIFICADO.
 botao_pesquisas.click()
+
+#####################################################################################################################
+
+# ESTE TRECHO SELECIONA O TOPICO INDICADORES
 
 # ENCONTRA O ELEMENTO COM A CLASSE "pesquisas-menu-arvore" NO DOM DA PÁGINA E ATRIBUI À VARIÁVEL
 pesquisas_menu_arvore = navegador.find_element(By.CLASS_NAME, "pesquisas-menu-arvore")
@@ -40,6 +52,9 @@ tag_li = tag_ul.find_element(By.TAG_NAME, "li")
 # O MUDULO REALIZA O CLIQUE NO PRIMEIRO ITEM DA LISTA <LI> ENCONTRADO
 tag_li.click()
 
+#####################################################################################################################
+
+# ESTE TRECHO SELECIONA O TOPICO INDUSTRIA
 
 # AGUARDA ATÉ QUE O BOTÃO COM O TEXTO "INDÚSTRIA" ESTEJA PRESENTE NO DOM DA PÁGINA.
 industria = WebDriverWait(navegador, 10).until(
@@ -49,6 +64,10 @@ industria = WebDriverWait(navegador, 10).until(
 industria.click() 
 # ARMAZENA O ELEMENTO ENCONTRADO NA VARIÁVEL industria E REALIZA O CLIQUE PARA INTERAGIR COM O BOTÃO.
 
+#####################################################################################################################
+
+# AQUI E SELECIONADO A PESQUISA INDUSTRIAL MENSAL - PIM - PF BRASIL
+
 # AGUARDA ATÉ QUE O LINK COM O TEXTO "PESQUISA INDUSTRIAL MENSAL - PRODUÇÃO FÍSICA - PIM-PF-BRASIL" ESTEJA PRESENTE NO DOM DA PÁGINA.
 pim_pf_brasil = WebDriverWait(navegador, 10).until(
     # USA O XPATH PARA LOCALIZAR O ELEMENTO <A> QUE CONTÉM O TEXTO ESPECÍFICO.
@@ -57,8 +76,110 @@ pim_pf_brasil = WebDriverWait(navegador, 10).until(
 # ARMAZENA O ELEMENTO ENCONTRADO NA VARIÁVEL pim_pf_brasil E REALIZA O CLIQUE PARA ACESSAR AS INFORMAÇÕES.
 pim_pf_brasil.click()
 
+#####################################################################################################################
+
+# ESTE TRECHO ENCONTRA E SELECIONA A TABELA 8887
+tabela = navegador.find_element(By.CLASS_NAME, "tabela-8887")
+tabela_element = tabela.find_elements(By.TAG_NAME, "td")
+
+# ATRAVES DO XPATH E ENCONTRADO O BOTÃO PARA SELEÇÃO DA TABELA
+botao_tabela = WebDriverWait(navegador, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="tab-tabelas"]/section/div[1]/table/tbody/tr[5]/td[3]/a'))
+)
+
+botao_tabela.click()
+
+#####################################################################################################################
+
+# ESTE TRECHO REALIZA A MARCAÇÃO NA TABELA VARIÁVEL DE TODAS AS VARIÁVEIS 
+
+# LOCALIZANDO O BOTÃO PELO ATRIBUTO 'DATA-CMD'
+button = WebDriverWait(navegador, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-cmd="marcarTudo"]'))
+)
+
+# GARANTINDO QUE O BOTÃO ESTEJA VISÍVEL ANTES DO CLIQUE
+navegador.execute_script("arguments[0].scrollIntoView(true);", button)
+
+# CLICANDO NO BOTÃO E SELECIONANDO TODAS AS OPÇÕES
+try:
+    button.click()
+except:
+    # FORÇANDO O CLIQUE VIA JS CASO O MÉTODO DIRETO VENHA A FALHAR
+    navegador.execute_script("arguments[0].click();", button)
+
+
+
+#####################################################################################################################
+
+# ESTE TRECHO REALIZA A MARCAÇÃO NA TABELA GRANDES CATEGORIAS ECONÔMICAS APENAS DE 1 BENS DE CAPITAL
+
+# LOCALIZANDO O BOTÃO PELO FULL XPATH
+button_2 = WebDriverWait(navegador, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div/div/div[1]/div[4]/div[3]/div/div[2]/div[3]/div/div[2]/div/div/div/div[2]/div[1]/div/div/div/button'))
+)
+try:
+    button_2.click()
+except:
+    # FORÇANDO O CLIQUE VIA JS CASO O METODO DIRETO VENHA A FALHAR
+    navegador.execute_script("arguments[0].click();", button_2)
+
+
+#####################################################################################################################
+
+# ESTE TRECHO REALIZA A MARCAÇÃO NA TABELA GRANDES CATEGORIAS ECONÔMICAS APENAS 2 BENS INTERMEDIÁRIOS
+button_2_1 = WebDriverWait(navegador, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div/div/div[1]/div[4]/div[3]/div/div[2]/div[3]/div/div[2]/div/div/div/div[2]/div[1]/div/div/div/button'))
+)
+try:
+    button_2_1.click()
+except:
+    # FORÇANDO O CLIQUE VIA JS CASO O METODO DIRETO VENHA A FALHAR
+    navegador.execute_script("arguments[0].click();", button_2_1)
+
+
+
+#####################################################################################################################
+
+# ESTE TRECHO REALIZA A MARCAÇÃO NA TABELA GRANDES CATEGORIAS ECONÔMICAS APENAS 3 BENS DE CONSUMO
+button_2_2 = WebDriverWait(navegador, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div/div/div[1]/div[4]/div[3]/div/div[2]/div[3]/div/div[2]/div/div/div/div[2]/div[4]/div/div/div/button'))
+)
+try:
+    button_2_2.click()
+except:
+    # FORÇANDO O CLIQUE VIA JS CASO O METODO DIRETO VENHA A FALHAR
+    navegador.execute_script("arguments[0].click();", button_2_2)
+
+
+
+#####################################################################################################################
+
+# ESTE TRECHO REALIZA A MARCAÇÃO NA TABELA GRANDES CATEGORIAS ECONÔMICAS APENAS 9 BENS NÃO ESPECIFICADOS ANTERIORMENTE
+button_2_3 = WebDriverWait(navegador, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div/div/div[1]/div[4]/div[3]/div/div[2]/div[3]/div/div[2]/div/div/div/div[2]/div[13]/div/div/div/button'))
+)
+try:
+    button_2_3.click()
+except:
+    # FORÇANDO O CLIQUE VIA JS CASO O METODO DIRETO VENHA A FALHAR
+    navegador.execute_script("arguments[0].click();", button_2_3)
+
+
+#####################################################################################################################
+
+button_2_4 = WebDriverWait(navegador, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div/div/div[1]/div[4]/div[4]/div/div[2]/div[3]/div/div[1]/div[1]/div/button[1]'))
+)
+try:
+    button_2_4.click()
+except:
+    # FORÇANDO O CLIQUE VIA JS CASO O METODO DIRETO VENHA A FALHAR
+    navegador.execute_script("arguments[0].click();", button_2_4)
+
+
+#####################################################################################################################
 
 
 input("Clique em ENTER para encerrar o navegador...")
 
-#time.sleep(20)
